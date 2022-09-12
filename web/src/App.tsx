34c4f281479391f0e52cae068967e4f4ec16e1d6
Rafael Simionato <rafaasimi@hotmail.com>
2022-09-12 19:23:56 -0300

    Arquivos iniciais web

---
+++ web/src/App.tsx
@@ -0,0 +1,22 @@
+// Componentes / Propriedades
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return <button>{props.title}</button>;
+}
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Button title="Send 1"/>
+      <Button title="Send 2"/>
+      <Button title="Send 3"/>
+    </>
+  );
+}
+
+export default App;
