d129251e67ead340ba5f483da51a58c6f4073b7e
Rafael Simionato <rafaasimi@hotmail.com>
2022-09-13 11:40:34 -0300

    Criação e estilização tela inicial e componentes

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,48 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StatusBar } from "react-native";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black,
+} from "@expo-google-fonts/inter";
 
-interface ButtonProps {
-  title: string;
-}
-
-function Button(props: ButtonProps) {
-  return (
-    <TouchableOpacity style={styles.button}>
-      <Text style={styles.text}>{props.title}</Text>
-    </TouchableOpacity>
-  )
-}
+import { Background } from "./src/components/Background";
+import { Home } from "./src/screens/Home";
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black,
+  });
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello World</Text>
-      <Text>Aplicação rodando no meu celular.</Text>
-      <Text>📱🎮</Text>
-      <Button title="BORA CODAR!"/>
-      <StatusBar style="auto"/>
-    </View>
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Home /> : <Loading />}
+    </Background>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  title: {
-    fontSize: 24
-  },
-  button: {
-    backgroundColor: 'purple',
-    padding: 10,
-    borderRadius: 8,
-    marginTop: 16
-  },
-  text: {
-    color: 'white'
-  }
-});
